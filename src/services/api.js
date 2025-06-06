e9699c77d2bc4583d532d823ae6a3029572ba242
Zephyrior <mark.temblique@gmail.com>
2025-06-06 10:25:31 +0200

    .

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { redirectToLogin } from "./navigation";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
-  //baseURL: "https://circle-xxxn.onrender.com/api",
+  //baseURL: "http://localhost:8080/api",
+  baseURL: "https://circle-xxxn.onrender.com/api",
 });
 
 api.interceptors.request.use(
